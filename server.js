211939a5720a4b6d223c7d2ac37ac50896811d10
Glen E. Ivey <not-an@email.addr>
2013-07-25 22:01:36 -0700

    [#53753793] change default port to :80

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ function respond(req, res, next) { res.send('hello ' + req.params.name); }
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(8080, function() {
+server.listen(80, function() {
   console.log('PairMatic %s listening at %s', server.name, server.url);
 });
