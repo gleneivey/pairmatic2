5db856e5e2f7aeb5a80ae8885fe8853843dde68a
Glen E. Ivey <not-an@email.addr>
2013-07-27 21:31:31 -0700

    [#53256639] add mongoDB/mongoose to project; server retrieves Tracker API Token from db on startup

---
+++ config-example.js
@@ -0,0 +1,1 @@
+exports.mongoUri = 'mongodb://localhost/pairmatic';
--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+var mongoose = require('mongoose/');
+var configurationSchema = mongoose.Schema({ apiToken: String });
+var Configuration = mongoose.model('Configuration', configurationSchema);
+var serverConfigurationData = null;
+
+var config = require('./config');
+mongoose.connect(config.mongoUri);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', getServerConfiguration);
+
+function getServerConfiguration() {
+  Configuration.findOne().exec(function(err, config) {
+    serverConfigurationData = config;
+  });
+}
+
+
+
+
 var restify = require('restify');
 var server = restify.createServer();
 
