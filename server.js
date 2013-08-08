2143dd9b8677ec2fd36b4be6a18b25bdcb6340ee
Glen E. Ivey <not-an@email.addr>
2013-08-07 18:19:58 -0700

    [#53267067] setup to serve static assets from 'client' directory

--- server.js
+++ server.js
@@ -42,9 +42,18 @@ function logResults() {
 var restify = require('restify');
 var server = restify.createServer();
 
+//server.get(/\/client\/.*/, restify.serveStatic({
+//    directory: './client/'
+//}));
+
 function respond(req, res, next) { res.send('hello ' + req.params.name); }
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
+
+server.get(/^\/.*$/, restify.serveStatic({
+    directory: './client/',
+    default: 'index.html'
+}));
 
 server.listen(8889, function() {
   console.log('PairMatic %s listening at %s', server.name, server.url);
