a51a7b7578e5c1840dbd33a76c194ac6f7ecb659
Glen E. Ivey <https://github.com/gleneivey>
2014-01-06 21:13:44 -0800

    refactor unit spec for pairing page view

--- spec/client/views/pairingPage.spec.js
+++ spec/client/views/pairingPage.spec.js
@@ -1,78 +1,80 @@
 describe("views.pairingPage", function() {
   var subject;
 
-  beforeEach(function() {
-    subject = new pairmatic.views.pairingPage();
-  });
-
-  it("uses the pairingPage template when rendered", function() {
-    spyOn(JST, 'client/js/templates/pairingPage.hbs').and.callReturn('<div/>');
-
-    var renderReturnValue = subject.render();
-
-    expect(renderReturnValue).toBe(subject);
-    expect(JST['client/js/templates/pairingPage.hbs']).toHaveBeenCalled();
-  });
-
-  it("should re-render when system state is updated", function() {
-    var people = pairmatic.models.people;
-    spyOn(people, 'toJSON').and.callReturn({});  // spy on this because render function is private
-    people.trigger('reset');
-    expect(people.toJSON).toHaveBeenCalled();
-  });
-
-  it("marks avatars as draggable when rendered", function() {
-    var spiedOnWrappedSelector;
-    setupSpyOnJquery('.avatar', function(spiedObject) {
-      spiedOnWrappedSelector = spiedObject;
-      spyOn(spiedObject, 'draggable');
-    });
-
-    subject.render();
-
-    expect(spiedOnWrappedSelector.draggable).toHaveBeenCalled();
-  });
-
-  describe("values passed to template", function() {
-    var capturedTemplateArgs;
-
+  describe("when rendered", funtion() {
     beforeEach(function() {
-      spyOn(JST, 'client/js/templates/pairingPage.hbs').and.callFake(
-          function(infoHash) { capturedTemplateArgs = infoHash; } );
+      subject = new pairmatic.views.pairingPage();
     });
 
-    it("should pass right active/inactive lists to template", function() {
-      var people = simplePeopleListFixture();
-      pairmatic.models.people.reset(people);
+    it("uses the pairingPage template", function() {
+      spyOn(JST, 'client/js/templates/pairingPage.hbs').and.callReturn('<div/>');
 
-      // only assert against fields we sent in....
-      capturedTemplateArgs.activeUsers =
-          _(capturedTemplateArgs.activeUsers  ).map(function(personHash) {
-              return { active: personHash.active, name: personHash.name }; });
-      capturedTemplateArgs.inactiveUsers =
-          _(capturedTemplateArgs.inactiveUsers).map(function(personHash) {
-              return { active: personHash.active, name: personHash.name }; });
+      var renderReturnValue = subject.render();
 
+      expect(renderReturnValue).toBe(subject);
       expect(JST['client/js/templates/pairingPage.hbs']).toHaveBeenCalled();
-      expect(capturedTemplateArgs).toEqual({
-	activeUsers:   [
-	  { active: true,  name: 'one' },
-	  { active: true,  name: 'four' }
-	],
-	inactiveUsers: [
-	  { active: false, name: 'two' },
-	  { active: false, name: 'three' }
-	]
-      });
     });
 
-    it("should send email addresses as their md5 hashes", function() {
-      var people = simplePeopleListFixture().slice(0,2);
-      pairmatic.models.people.reset(people);
+    it("it responds to model updates", function() {
+      var people = pairmatic.models.people;
+      spyOn(people, 'toJSON').and.callReturn({});  // spy on this because render function is private
+      people.trigger('reset');
+      expect(people.toJSON).toHaveBeenCalled();
+    });
 
-      expect(JST['client/js/templates/pairingPage.hbs']).toHaveBeenCalledWith({
-	activeUsers:   [{ active: true,  name: 'one', email: hex_md5('one@example.com') }],
-	inactiveUsers: [{ active: false, name: 'two', email: hex_md5('two@three.four') }]
+    it("it marks avatars as draggable", function() {
+      var spiedOnWrappedSelector;
+      setupSpyOnJquery('.avatar', function(spiedObject) {
+	spiedOnWrappedSelector = spiedObject;
+	spyOn(spiedObject, 'draggable');
+      });
+
+      subject.render();
+
+      expect(spiedOnWrappedSelector.draggable).toHaveBeenCalled();
+    });
+
+    describe("values passed to template", function() {
+      var capturedTemplateArgs;
+
+      beforeEach(function() {
+	spyOn(JST, 'client/js/templates/pairingPage.hbs').and.callFake(
+	    function(infoHash) { capturedTemplateArgs = infoHash; } );
+      });
+
+      it("should pass right active/inactive lists to template", function() {
+	var people = simplePeopleListFixture();
+	pairmatic.models.people.reset(people);
+
+	// only assert against fields we sent in....
+	capturedTemplateArgs.activeUsers =
+	    _(capturedTemplateArgs.activeUsers  ).map(function(personHash) {
+		return { active: personHash.active, name: personHash.name }; });
+	capturedTemplateArgs.inactiveUsers =
+	    _(capturedTemplateArgs.inactiveUsers).map(function(personHash) {
+		return { active: personHash.active, name: personHash.name }; });
+
+	expect(JST['client/js/templates/pairingPage.hbs']).toHaveBeenCalled();
+	expect(capturedTemplateArgs).toEqual({
+	  activeUsers:   [
+	    { active: true,  name: 'one' },
+	    { active: true,  name: 'four' }
+	  ],
+	  inactiveUsers: [
+	    { active: false, name: 'two' },
+	    { active: false, name: 'three' }
+	  ]
+	});
+      });
+
+      it("should send email addresses as their md5 hashes", function() {
+	var people = simplePeopleListFixture().slice(0,2);
+	pairmatic.models.people.reset(people);
+
+	expect(JST['client/js/templates/pairingPage.hbs']).toHaveBeenCalledWith({
+	  activeUsers:   [{ active: true,  name: 'one', email: hex_md5('one@example.com') }],
+	  inactiveUsers: [{ active: false, name: 'two', email: hex_md5('two@three.four') }]
+	});
       });
     });
   });
